389c149b90bcc12d97bed0a1b5fe253cb2b4620b
HritzWalunj <hritujawalunj@gmail.com>
2026-01-08 15:06:40 +0530

    make it simple

--- advance_calculator/advance_calculator.py
+++ advance_calculator/advance_calculator.py
@@ -1,20 +1,17 @@
-from calculator.basic.calculator.calculator import add, mul, div
 
 def power(a, b):
     return a ** b
 
 def square(a):
-    return mul(a , a)
+    return a * a
 
 def cube(a):
     return a * a * a
 
 def average(a, b):
-    return div(add(a, b), 2)
+    return (a + b) / 2
 
 def percentage(value, total):
     if total == 0:
         raise ValueError("Total cannot be zero")
-    return div(value , total) * 100
-
-print(f"square= {square(3)}")
+    return value / total * 100
---
+++ tests/test_advance.py
@@ -0,0 +1,16 @@
+from advance_calculator.advance_calculator import power, square, average, percentage
+
+def test_power():
+    assert power(2 , 3) == 8
+
+def test_square():
+    assert square(2) == 4
+
+def test_cube():
+    assert cube(3) == 27
+
+def test_average():
+    assert average(2, 4) == 3
+
+def test_percentage():
+    assert percentage(22, 100) == 22
