54d0133309588cb7a6195c38025bd086da6fc583
HritzWalunj <hritujawalunj@gmail.com>
2026-01-12 15:11:12 +0530

    fixed indentation error

--- advance_calculator/advance_calculator.py
+++ advance_calculator/advance_calculator.py
@@ -5,19 +5,19 @@ def power(a, b):
     return a ** b
 
 def square(a):
-        """retrun a given power of a number"""
+    """retrun a square of a number"""
     return a * a
 
 def cube(a):
-    """retrun a given power of a number"""
+    """retrun a cube of a number"""
     return a * a * a
 
 def average(a, b):
-    """retrun a given power of a number"""
+    """retrun a average of a numbers"""
     return (a + b) / 2
 
 def percentage(value, total):
-    """retrun a given power of a number"""
+    """retrun percentage"""
     if total == 0:
         raise ValueError("Total cannot be zero")
     return value / total * 100
